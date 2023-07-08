e483e29bfc608a260946f67214e5093dcf2c91d2
bishwamohan14 <bishwamohan14@gmail.com>
2023-07-08 21:31:08 +0530

    IPL dashboard but only backend crud operation

---
+++ src/main/java/com/Project2023/IplDashBoard/IplDashBoardApplication.java
@@ -0,0 +1,14 @@
+package com.Project2023.IplDashBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashBoardApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/Project2023/IplDashBoard/IplDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project2023.IplDashBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
